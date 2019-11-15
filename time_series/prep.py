130dbb375c6204341e7ff0c58aea1e09db95a83c
JefferyRoeder <jefferyroeder@gmail.com>
2019-11-15 16:58:45 -0600

    time series exploratory and prep

---
+++ time_series/prep.py
@@ -0,0 +1,23 @@
+import pandas as pd
+def get_stores_data():
+    items = pd.read_csv('items.csv')
+    sales = pd.read_csv('sales.csv')
+    stores = pd.read_csv('stores.csv')
+    df = sales.merge(stores,left_on='store',right_on='store_id',how='left')
+    df = df.merge(items,left_on='item',right_on='item_id',how='left')
+    df.drop(columns=['store_id','item_id'],inplace=True)
+    df = sales.merge(stores,left_on='store',right_on='store_id',how='left')
+    df = df.merge(items,left_on='item',right_on='item_id',how='left')
+    df.drop(columns=['store_id','item_id'],inplace=True)
+    fmt = '%a, %d %b %Y %H:%M:%S %Z'
+    df.sale_date = pd.to_datetime(df.sale_date,format=fmt)
+    df = df.set_index('sale_date')
+    df['month'] = df.index.month
+    df['weekday'] = df.index.weekday
+    df['sales_total'] = df.sale_amount * df.item_price
+    return df
+
+def sales_by_day(df):
+    sales_by_day = df.resample('D')[['sales_total']].sum()
+    sales_by_day['diff_with_last_day'] = sales_by_day.sales_total.diff()
+    return sales_by_day
