4581f85b12aea8c0b5eee474e9b7a14a966c8e30
JefferyRoeder <jefferyroeder@gmail.com>
2019-12-16 16:57:22 -0600

    pyspark mini exercises

---
+++ nlp/acquire2.py
@@ -0,0 +1,5 @@
+
+
+# project specific imports
+from acquire_codeup_blog import get_blog_posts
+from acquire_news_articles import get_news_articles
--- nlp/prep.py
+++ nlp/prep.py
@@ -62,3 +62,51 @@ def remove_stopwords(string, extra_words=[], exclude_words=[]):
     filtered_words = [w for w in words if w not in stopword_list]
     final_string = " ".join(filtered_words)
     return final_string
+
+def normalize2(string):
+    """
+    Convert to all lowercase  
+    Normalize the unicode chars  
+    Remove any non-alpha or whitespace characters  
+    Remove any alpha strings with 2 characters or less  
+    """
+    string = string.lower()
+    string = unicodedata.normalize('NFKD', string).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    
+    # keep only alpha chars
+    string = re.sub(r'[^a-z]', ' ', string)
+    
+    # remove strings less than 2 chars in length
+    string = re.sub(r'\b[a-z]{,2}\b', '', string)
+    
+    # convert newlines and tabs to a single space
+    string = re.sub(r'[\r|\n|\r\n]+', ' ', string)
+    
+    # strip extra whitespace
+    string = string.strip()
+    
+    return string
+
+def stem2(string):
+    ps = nltk.porter.PorterStemmer()
+    stems = [ps.stem(word) for word in string.split()]
+    string_of_stems = ' '.join(stems)
+    return string_of_stems
+
+def remove_stopwords2(tokenized_string, extra_words=[], exclude_words=[]):
+    words = tokenized_string.split()
+    stopword_list = stopwords.words('english')
+
+    # remove the excluded words from the stopword list
+    stopword_list = set(stopword_list) - set(exclude_words)
+
+    # add in the user specified extra words
+    stopword_list = stopword_list.union(set(extra_words))
+
+    filtered_words = [w for w in words if w not in stopword_list]
+    final_string = " ".join(filtered_words)
+    return final_string
+
+def tokenize2(string):
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+    return tokenizer.tokenize(string, return_str=True)
